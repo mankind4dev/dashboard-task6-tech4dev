6e9e65cf6cd0400477ba420adaeeb153cdb5c6f9
mankind4dev <ogunmakin120@gmail.com>
2024-11-04 12:46:58 +0100

    SLice updated

--- src/redux/dashboardSlice/sidebarSlice.js
+++ src/redux/dashboardSlice/sidebarSlice.js
@@ -5,12 +5,6 @@ import library from "/images/sidebar/library.png"
 import setting from "/images/sidebar/setting.png"
 import getStarted from "/images/sidebar/getStarted.png"
 
-import ReportIcon from "../../icons/reportIcon";
-import LibraryIcon from "../../icons/libraryIcon";
-import PeopleIcon from "../../icons/peopleIcon";
-import Activities from "../../icons/activities";
-import GetStartedIcon from "../../icons/getStartedIcon";
-import SettingIcon from "../../icons/settingIcon";
 
 const initialState = {
   value: 0,
@@ -18,25 +12,22 @@ const initialState = {
     {
       title: "Report",
       iconImg: active,
-      // icon: <ReportIcon />,
       link: "/report",
     },
     {
       title: "Library",
       iconImg: library,
-      // icon: <LibraryIcon />,
+
       link: "/libraries",
     },
     {
       title: "People",
       iconImg: people,
-      // icon: <PeopleIcon />,
       link: "/people",
     },
     {
       title: "Activities",
       iconImg: active,
-      // icon: <Activities />,
       link: "/activities",
     },
     {
@@ -48,13 +39,13 @@ const initialState = {
     {
       name: "Get Started",
       iconImg: getStarted,
-      // icon: <GetStartedIcon />,
+      
       link: "/get-started",
     },
     {
       name: "Settings",
       iconImg: setting,
-      // icon: <SettingIcon />,
+
       link: "/settings",
     },
   ],
